2b3598d9b0c2f86f9f90c2948a6ccb3151b56aac
Alex Mauricio <amgdark@uaz.edu.mx>
2017-10-05 09:57:56 -0500

    agrega comillas

--- script.sh
+++ script.sh
@@ -7,11 +7,11 @@ echo "Hola desde script"
 echo "-----------------------------------------------------------"
 
 
-if [[ $TRAVIS_PYTHON_VERSION == 3.4 ]]; then 
+if [[ "$TRAVIS_PYTHON_VERSION" == "3.4" ]]; then 
 	echo -e "Python3 \ntodas las sentencias que se quieran \nejecutar con python 3"
 
 fi
-if [[ $TRAVIS_PYTHON_VERSION == 2.7 ]]; then 
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then 
 	echo "Python2 \n todas las sentencias que se quieran \nejecutar con python 2"
 
 	python TestCalculadora.py
