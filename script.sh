61bff973977f5407940a7c166914d6d1d526fe37
Alex Mauricio <amgdark@uaz.edu.mx>
2017-10-05 08:56:38 -0500

    agrega los dos corchetes que maneja travis en el IF para las variables de entorno

--- script.sh
+++ script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 echo "Hola desde script"
 echo "-----------------------------------------------------------"
 
-if [ $TRAVIS_PYTHON_VERSION == 3* ]; then 
+if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then 
 	echo "Python3 \n todas las sentencias que se quieran \nejecutar con python 3"
 
 fi
-if [ $TRAVIS_PYTHON_VERSION == 2* ]; then 
+if [[ $TRAVIS_PYTHON_VERSION == 2* ]]; then 
 	echo "Python2 \n todas las sentencias que se quieran \nejecutar con python 2"
 
 	python TestCalculadora.py
